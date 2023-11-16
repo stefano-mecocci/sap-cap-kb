61bdadcf403dcef30ce3c867c8bf75934bef2800
Stefano Mecocci <mecocci15@gmail.com>
2023-11-16 23:17:16 +0100

    first deploy with vitepress + gh pages!

--- .vitepress/dist/assets/faq.md.9568d0e4.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,o as s,c as e,Q as o}from"./chunks/framework.36bc40e2.js";const f=JSON.parse('{"title":"F.A.Q.","description":"","frontmatter":{},"headers":[],"relativePath":"faq.md","filePath":"faq.md"}'),n={name:"faq.md"},l=o("",4),t=[l];function p(i,r,c,d,E,u){return s(),e("div",null,t)}const h=a(n,[["render",p]]);export{f as __pageData,h as default};
